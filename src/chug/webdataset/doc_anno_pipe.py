d9b4c5db345c2c6a693f2b787e3e2a41950616a7
Ross Wightman <rwightman@gmail.com>
2023-06-22 23:28:18 -0700

    Add ability to read and render pdf files

--- src/chug/webdataset/doc_anno_pipe.py
+++ src/chug/webdataset/doc_anno_pipe.py
@@ -8,9 +8,23 @@ import webdataset as wds
 
 from PIL import Image
 
+
+# IMPORTANT fitz aka PyMuPDF is AGPL licensed w/ a commercial purchased option
+# manual intervention required to use it.
+_USE_AGPL_PYMUPDF = False
+
+if _USE_AGPL_PYMUPDF:
+    try:
+        import fitz
+    except ImportError as e:
+        fitz = None
+else:
+    fitz = None
+
 from .loader import log_and_continue
 
 _logger = logging.getLogger(__name__)
+
 
 def filter_no_annotation_or_no_image(sample):
     # FIXME check sample for valid doc/image and annotation
@@ -34,7 +48,75 @@ class DocProcessor:
         self.squeeze_pages = True
         self.generator = random.Random()
         self.generator.seed(seed)
-        #print(seed, self.generator.getstate())
+        # FIXME note, should move to torchvision v2 annotations at some point, they should
+        #  have a generator arg (eventually) which will make proper state restore possible
+
+    def _decode_image_pages(
+            self,
+            sample,
+            ext,
+            page_indices,
+            num_anno_pages,
+    ):
+        with io.BytesIO(sample[ext]) as b:
+            image = Image.open(b)
+            num_image_pages = getattr(image, 'n_frames', 1)
+            if num_image_pages != num_anno_pages:
+                _logger.warning(
+                    f'Mismatch between num image and num annotation pages {num_image_pages} != {num_anno_pages}'
+                    f' for sample {sample["__url__"]}, {sample["__key__"]}.')
+            pages = []
+            for i, page_index in enumerate(page_indices):
+                if num_image_pages > 1:
+                    image.seek(page_index)
+                else:
+                    assert num_anno_pages == 1
+                    image.load()
+
+                if self.image_fmt:
+                    image = image.convert(self.image_fmt)
+
+                # if page_image_info is not None:
+                #     # FIXME, if train objective involves masking or otherwise processing image
+                #     #  with knowledge of annotations / text content, anno info should contain
+                #     #  mask locations, etc. For such a task, we need to pass it to image preprocess
+                #     image = self.image_preprocess(image, page_info=page_image_info[i])
+                # else:
+                image = self.image_preprocess(image)
+            pages += [image]
+        return image, num_image_pages
+
+    def _decode_pdf_pages(
+            self,
+            sample,
+            ext,
+            page_indices,
+            num_anno_pages,
+    ):
+        with io.BytesIO(sample[ext]) as b:
+            # FIXME test and use an alternate pdf reader/render as default
+            assert fitz is not None, "fitz (pymupdf) is not installed and enabled"
+            doc = fitz.Document(stream=b)
+            num_image_pages = doc.page_count
+            pages = []
+            for i, page_index in enumerate(page_indices):
+                page = doc.load_page(page_index)
+                pixmap = page.get_pixmap(dpi=150)
+                image = Image.frombuffer('RGB', (pixmap.width, pixmap.height), pixmap.samples)
+
+                if self.image_fmt:
+                    image = image.convert(self.image_fmt)
+
+                # if page_image_info is not None:
+                #     # FIXME, if train objective involves masking or otherwise processing image
+                #     #  with knowledge of annotations / text content, anno info should contain
+                #     #  mask locations, etc. For such a task, we need to pass it to image preprocess
+                #     image = self.image_preprocess(image, page_info=page_image_info[i])
+                # else:
+                image = self.image_preprocess(image)
+            pages += [image]
+
+        return pages, num_image_pages
 
     def __call__(self, sample):
         anno = json.loads(sample['json'])
@@ -52,45 +134,36 @@ class DocProcessor:
             page_indices = info.get('page_indices', [0])  # the samples page indices
             num_decode_pages = len(page_indices)
             num_anno_pages = info.get('num_pages', 1)
-            page_image_info = info.get('image_info', None)
-            if page_image_info is not None:
-                assert len(page_image_info) == len(page_indices)
+            # page_image_info = info.get('image_info', None)
+            # if page_image_info is not None:
+            #     assert len(page_image_info) == len(page_indices)
         else:
             num_decode_pages = num_anno_pages = 1
             page_indices = [0]
-            page_image_info = None
+            # page_image_info = None
 
         # decode page images
         page_images = []
         for ext in self.image_ext:
             if ext in sample:
-                with io.BytesIO(sample[ext]) as b:
-                    image = Image.open(b)
-                    num_image_pages = getattr(image, 'n_frames', 1)
-                    if num_image_pages != num_anno_pages:
-                        _logger.warning(
-                            f'Mismatch between num image and num annotation pages {num_image_pages} != {num_anno_pages}'
-                            f' for sample {sample["__url__"]}, {sample["__key__"]}.')
-                    for i, page_index in enumerate(page_indices):
-                        if num_image_pages > 1:
-                            image.seek(page_index)
-                        else:
-                            assert num_anno_pages == 1
-                            image.load()
-
-                        if self.image_fmt:
-                            image = image.convert(self.image_fmt)
-
-                        if page_image_info is not None:
-                            # FIXME, if train objective involves masking or otherwise processing image
-                            #  with knowledge of annotations / text content, anno info should contain
-                            #  mask locations, etc. For such a task, we need to pass it to image preprocess
-                            image = self.image_preprocess(image, page_info=page_image_info[i])
-                        else:
-                            image = self.image_preprocess(image)
-                        # FIXME note, should move to torchvision v2 annotations at some point, they should
-                        #  have a generator arg (eventually) which will make proper state restore possible
-                        page_images.append(image)
+                image_bytes = sample[ext]
+                if ext == 'pdf':
+                    images, num_image_pages = self._decode_pdf_pages(
+                        sample,
+                        ext,
+                        page_indices,
+                        num_anno_pages,
+                    )
+                else:
+                    images, num_image_pages = self._decode_image_pages(
+                        sample,
+                        ext,
+                        page_indices,
+                        num_anno_pages,
+                    )
+                page_images.extend(images)
+                # process one document type per doc, should be ordered by priority
+                break
 
         assert len(page_images), 'No page images present'
 
